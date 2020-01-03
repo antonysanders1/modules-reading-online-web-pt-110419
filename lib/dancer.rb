ddfc4bfab878b6a4b6c0d67b1f0fbedcaf7f3bb7
Antony Sanders <antony.devon92@gmail.com>
2020-01-03 19:41:17 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,7 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Dancer 
+class Dancer
   include Dance
  
   attr_accessor :name
@@ -9,5 +9,4 @@ class Dancer
   def initialize(name)
     @name = name
   end
-  
-end 
+end
